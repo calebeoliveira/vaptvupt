2239f7ca1260b7fe9e7ec842d9137486c633cb23
Calebe Oliveira <becalebe@gmail.com>
2015-11-13 00:25:00 -0300

    Added collapse side nav for mobile

--- resources/views/home.php
+++ resources/views/home.php
@@ -39,5 +39,6 @@
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.min.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js"></script>
 	<script type="text/javascript" src="js/app.js"></script>
+	<script type="text/javascript">$('.button-collapse').sideNav();</script>
 </body>
 </html>
