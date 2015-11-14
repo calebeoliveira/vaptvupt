d11712692bb25e18c7eb2720ace325d5a33afcb8
Calebe Oliveira <becalebe@gmail.com>
2015-11-14 19:13:22 -0300

    Orders SPA done

--- resources/angular/controllers/OrdersCtrl.js
+++ resources/angular/controllers/OrdersCtrl.js
@@ -1,2 +1,15 @@
 vv.controller('OrdersCtrl', function($scope) {
+
+	$scope.orders = [
+		{
+			amount: 40.5,
+			createdAt: '16:30h',
+			items: [
+				'Muqueca de camarão (1x) R$30.00',
+				'Poção de arroz (1x) R$3.00',
+				'Coca cola 2L (1x) R$4.50',
+				'Suco de fruta 1L (1x) R$3.00'
+			]
+		}
+	];
 });
